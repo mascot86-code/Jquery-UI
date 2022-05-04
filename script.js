4058c723fd23923f468e9c297899b972e7ec5d35
mascot86 <mascot86@mail.ru>
2022-05-04 15:33:35 +0300

    add counter widget

--- script.js
+++ script.js
@@ -27,4 +27,18 @@ $('document').ready(function () {
     $('.to_top').on('click', function () {
         $("html, body").animate({ scrollTop: 0 }, 50)
     })
+
+    //Counter
+
+    $('.counter').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now))
+            }
+        })
+    })
 })
